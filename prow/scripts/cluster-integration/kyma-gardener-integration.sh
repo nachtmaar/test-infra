e7010d8a5942de332af6943ecbebbee0c67d8c91
Nils Schmidt <nils.schmidt02@sap.com>
2020-02-12 15:42:41 +0100

    Add time for debugging

--- prow/scripts/cluster-integration/kyma-gardener-integration.sh
+++ prow/scripts/cluster-integration/kyma-gardener-integration.sh
@@ -32,6 +32,9 @@ fi
 #Exported variables
 export TEST_INFRA_SOURCES_DIR="${KYMA_PROJECT_DIR}/../test-infra"
 export TEST_INFRA_CLUSTER_INTEGRATION_SCRIPTS="${TEST_INFRA_SOURCES_DIR}/prow/scripts/cluster-integration/helpers"
+
+sleep 3600
+
 # shellcheck disable=SC1090
 source "${TEST_INFRA_SOURCES_DIR}/prow/scripts/library.sh"
 # shellcheck disable=SC1090
